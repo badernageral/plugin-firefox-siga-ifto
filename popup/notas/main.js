336d59bdba9382f41f652c2391fbe36d28e725ce
José Robson Mariano Alves <badernageral@hotmail.com>
2021-04-27 10:22:44 -0300

    ...

--- popup/notas/main.js
+++ popup/notas/main.js
@@ -6,6 +6,18 @@ document.addEventListener("click", function (e) {
 		document.getElementById("lista").value.trim().split("\n").forEach(function (item, indice, array) {
 			lista.push(item.trim().split(/\s+/));
 		});
+		for (j = 0; j < lista.length; j++) {
+			if(lista[j][1]>10){
+				alert("Você não pode informar notas maiores que 10!");
+				return;
+			}
+			if(lista[j][2]!=undefined){
+				if (lista[0][2] > 10) {
+					alert("Você não pode informar notas maiores que 10!");
+					return;
+				}
+			}
+		}
 		let executando = browser.tabs.executeScript({
 			file: "magic.js"
 		});
