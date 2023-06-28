e98e3c270230e5ff645f5860023d3e1f02ad995f
badernageral <badernageral@hotmail.com>
2023-06-28 15:41:30 -0300

    1.3.3

--- popup/presenca/magic.js
+++ popup/presenca/magic.js
@@ -12,6 +12,7 @@
 		var alunos_nao_encontrados = "";
 		var alunos_duplicados = "";
 		var alunos_encontrados = 0;
+		var alunos_trancados=0;
 		var presenca = 0;
 		var falta = 0;
 		var matriculas = document.querySelectorAll(".hide-sm");
@@ -20,21 +21,23 @@
 			var qtd_encontrado = 0;
 			for (var i = 0; i < matriculas.length; i++) {
 				if (matriculas[i].children[0].innerHTML.trim().toLowerCase()==alunos[j][0].trim().toLowerCase()) {
-					if (alunos[j][1].trim() == "-") {
-						falta++;
-					} else {
-						presenca++;
-					}
 					var notas = matriculas[i].parentNode.parentNode.querySelectorAll("td.text-center");
 					for(var k=0;k<notas.length;k++){
 						if (alunos[j][k+1] != undefined){
-							if (alunos[j][k+1].trim() == "-") {
-								let quantidade_aulas = document.querySelector("#table_faltas").childNodes[0].childNodes[0].childNodes[k+1].childNodes[1].innerHTML.match(/(\d+)/)[0];
-								notas[k].childNodes[0].childNodes[0].value = quantidade_aulas;
-							} else {
-								notas[k].childNodes[0].childNodes[0].value = "0";
+							if(notas[k].childNodes[0].childNodes[0]==undefined){
+								alunos_trancados++;
+								break;
+							}else{
+								if (alunos[j][k+1].trim() == "-") {
+									let quantidade_aulas = document.querySelector("#table_faltas").childNodes[0].childNodes[0].childNodes[k+1].childNodes[1].innerHTML.match(/(\d+)/)[0];
+									notas[k].childNodes[0].childNodes[0].value = quantidade_aulas;
+									falta++;
+								} else {
+									notas[k].childNodes[0].childNodes[0].value = "0";
+									presenca++;
+								}
+								notas[k].childNodes[0].childNodes[0].dispatchEvent(new Event('blur'));
 							}
-							notas[k].childNodes[0].childNodes[0].dispatchEvent(new Event('blur'));
 						}
 					}
 					alunos_encontrados++;
@@ -55,7 +58,7 @@
 		if (alunos_duplicados != "") {
 			alunos_duplicados = "\n\nAlunos duplicados:" + alunos_duplicados;
 		}
-		alert("Você informou " + alunos.length + " alunos.\nForam encontrados " + alunos_encontrados + " alunos." + alunos_nao_encontrados + alunos_duplicados + "\n\nPresenças: " + presenca + "\nFaltas: " + falta);
+		alert("Você informou " + alunos.length + " alunos.\nForam encontrados " + alunos_encontrados + " alunos." + alunos_nao_encontrados + "\nTrancados: " + alunos_trancados + alunos_duplicados + "\n\nPresenças: " + presenca + "\nFaltas: " + falta);
 	}
 
 	browser.runtime.onMessage.addListener(mensagemRecebida);
